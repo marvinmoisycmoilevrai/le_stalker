1814c41d28430d71cfefe4227e442e56b3d20fb5
Marvin <marvinmoisy@gmail.com>
2018-03-17 11:34:32 +0100

    la ça marche (enfin testez pour vous, je sais la clé est coté client j'y travail

---
+++ le_stalker/serveur.js
@@ -0,0 +1,64 @@
+var fs = require('fs'),
+  https = require('https'),
+  express = require('express'),
+  app = express();
+
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}, app).listen(8080);
+
+app.get('/', function(req, res) {
+	fs.readFile("./index.html", function(err, data) {
+	  if (err) {
+	    res.writeHead(404, {'Content-Type': 'text/html'});
+	    return res.end("404 Not Found");
+	  }
+	  res.writeHead(200, {'Content-Type': 'text/html'});
+	  res.write(data);
+	  return res.end();
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
+//
+// http.createServer(function (req, res) {
+// 	fs.readFile("./index.html", function(err, data) {
+// 	  if (err) {
+// 	    res.writeHead(404, {'Content-Type': 'text/html'});
+// 	    return res.end("404 Not Found");
+// 	  }
+// 	  res.writeHead(200, {'Content-Type': 'text/html'});
+// 	  res.write(data);
+// 	  return res.end();
+// 	});
+// }).listen(8080);
+
+
+
+// var query = url.parse(req.url, true);
+// var filename = "." + q.pathname;
+// fs.readFile(filename, function(err, data) {
+//   if (err) {
+//     res.writeHead(404, {'Content-Type': 'text/html'});
+//     return res.end("404 Not Found");
+//   }
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write(data);
+//   return res.end();
+// });
