418703953a60199deee4723ccf5c071b657f0923
Marvin <marvinmoisy@gmail.com>
2018-03-21 13:34:18 +0100

    ça avance

--- Django/stalker/backoffice/views.py
+++ Django/stalker/backoffice/views.py
@@ -1,15 +1,13 @@
 
 from django.views.generic import *
 
+APP_ID = '929000163934820'
+
 class HomeView(TemplateView):
 
     template_name="backoffice/templates/home.html"
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['firstname'] = "olivier"
-        context['lastname'] = "engel"
-        context['towns'] = ["Mulhouse", "Strasbourg", "Marseille"]
-        context['data'] = {"age": 30, "genre": "male", "hobbies": "python"}
-        context['years'] = (1900, 2000, 2014)
+        context['appid'] = APP_ID
         return context
--- Django/stalker/stalker/settings.py
+++ Django/stalker/stalker/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = './stalker/static/'
 #
 # TEMPLATE_DIRS = [
 #     os.path.realpath('.') +"/templates"
