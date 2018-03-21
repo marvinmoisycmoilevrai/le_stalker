48187e3b20e687333f2d7a5c9639bba7d42e3c22
Marvin <marvinmoisy@gmail.com>
2018-03-21 14:02:54 +0100

    essai encore

--- Django/stalker/stalker/settings.py
+++ Django/stalker/stalker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'backoffice',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = './stalker/static/'
+STATIC_URL = '/static/'
 #
 # TEMPLATE_DIRS = [
 #     os.path.realpath('.') +"/templates"
